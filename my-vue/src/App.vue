aac6ecacaa702694944478c930392ba332654df9
ky213 <y.keddaoui@gmail.com>
2018-06-23 10:36:01 +0100

    init

---
+++ my-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container-fluid">
+    <Converter/>
+  </div>
+</template>
+
+<script>
+  import Converter from "./components/Converter.vue";
+  
+  export default {
+    name: "app",
+    components: {
+      Converter
+    }
+  };
+</script>
+
+<style lang="scss">
+  
+</style>
---
+++ my-vue/src/components/Converter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row mt-5">
+    <form class="col-5 m-auto">
+      <div class="form-group">
+        <label for="fromeCurrency">From</label>
+        <input type="text" class="form-control" id="fromCurrency" aria-describedby="emailHelp" placeholder="0.00">
+      </div>
+      <div class="form-group">
+        <label for="toCurrency">To</label>
+        <input type="text" class="form-control" id="toCurrency" placeholder="0.00">
+      </div>
+      <button type="submit" class="btn btn-primary">Go!</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+  
+  };
+</script>
+
+<style>
+  
+</style>
