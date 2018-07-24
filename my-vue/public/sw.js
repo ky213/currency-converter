4c2c33a114e331dd405ae1840da84f5366492de9
ky213 <y.keddaoui@gmail.com>
2018-07-24 12:56:32 +0100

    service worker and indexDB

---
+++ my-vue/public/sw.js
@@ -0,0 +1,50 @@
+var staticCacheName = "pwa-currency-converter-v1";
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      cache
+        .addAll([
+          "/",
+          "js/app.565676f5.js",
+          "js/chunk-vendors.8e15b4b0.js",
+          "css/app.e12af5ed.css",
+          "css/chunk-vendors.3f7932d1.css",
+          "https://restcountries.eu/rest/v2/all?fields=name;flag;currencies"
+        ])
+        .then(async function() {
+          const result = await cache.match(
+            "https://restcountries.eu/rest/v2/all?fields=name;flag;currencies"
+          );
+          const countries = await result.json();
+          const flagList = countries.map(({ flag }) => flag);
+          cache.addAll(flagList);
+        });
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  var url = new URL(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cachNames) {
+      console.log("activated", cachNames);
+      cachNames.forEach(function(cachName) {
+        if (
+          cachName.startsWith("pwa-currency-converter") &&
+          cachName !== staticCacheName
+        )
+          caches.delete(cachName);
+      });
+    })
+  );
+});
--- my-vue/src/components/CountryList.vue
+++ my-vue/src/components/CountryList.vue
@@ -12,68 +12,90 @@
 </template>
 
 <script>
-    import axios from 'axios'
-    
-    export default {
-        data: function() {
-            return {
-                allCountries: [],
-                popularCountries: []
-            }
-        },
-        methods: {
-            selectCountry(event) {
-                const countryName = event.target.id
-                const country = this.allCountries.concat(this.popularCountries).find((country) => {
-                    if (country.name === countryName)
-                        return country
-                })
-                const currencyCode = country.currencies[0].code
-                const currencySymbol = country.currencies[0].symbol
-                this.$emit('selectCountry', {
-                    countryName,
-                    currencyCode,
-                    currencySymbol,
-                })
-    
-            }
-        },
-        beforeMount: async function() {
-            const countries = await axios('https://restcountries.eu/rest/v2/all?fields=name;flag;currencies')
-            const allCountries = countries.data.slice()
-            const popularCountries = countries.data.filter((country) => {
-                return country.name.match(/United States of America|China|United Kingdom|Japan|Canada/)
-            })
-            this.allCountries = allCountries.filter((country) =>  !country.name.match(/United States of America|China|United Kingdom|Japan|Canada/)  )
-            this.popularCountries = popularCountries.reverse()
-        },
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      allCountries: [],
+      popularCountries: []
+    };
+  },
+  methods: {
+    selectCountry(event) {
+      const countryName = event.target.id;
+      const country = this.allCountries
+        .concat(this.popularCountries)
+        .find(country => {
+          if (country.name === countryName) return country;
+        });
+      const currencyCode = country.currencies[0].code;
+      const currencySymbol = country.currencies[0].symbol;
+      this.$emit("selectCountry", {
+        countryName,
+        currencyCode,
+        currencySymbol
+      });
     }
+  },
+  beforeMount: async function() {
+    const countries = await axios(
+      "https://restcountries.eu/rest/v2/all?fields=name;flag;currencies"
+    );
+    const allCountries = countries.data.slice();
+    const popularCountries = countries.data.filter(country => {
+      return country.name.match(
+        /United States of America|China|United Kingdom|Japan|Canada/
+      );
+    });
+
+    popularCountries.push({
+      name: "Europe",
+      currencies: [
+        {
+          code: "EUR",
+          name: "Euro",
+          symbol: "€"
+        }
+      ],
+      flag:"img/europe.png"
+    });
+
+    this.allCountries = allCountries.filter(
+      country =>
+        !country.name.match(
+          /United States of America|China|United Kingdom|Japan|Canada/
+        )
+    );
+    this.popularCountries = popularCountries.reverse()
+  }
+};
 </script>
 
 <style lang="scss">
-    ul {
-        margin-top: -17px;
-        padding: 0;
-        width: 95%;
-        max-height: 200px;
-        overflow: auto;
-        border: 1px solid #ced4da;
-        background: lightsteelblue;
-        z-index: 1;
-        li {
-            list-style-type: none;
-            padding: 5px;
-            cursor: pointer;
-            img {
-                width: 30px;
-            }
-        }
-        li:nth-child(odd) {
-            background: whitesmoke
-        }
-        li:hover {
-            background: rgb(223, 220, 220);
-            color: firebrick
-        }
+ul {
+  margin-top: -17px;
+  padding: 0;
+  width: 95%;
+  max-height: 200px;
+  overflow: auto;
+  border: 1px solid #ced4da;
+  background: lightsteelblue;
+  z-index: 1;
+  li {
+    list-style-type: none;
+    padding: 5px;
+    cursor: pointer;
+    img {
+      width: 30px;
     }
+  }
+  li:nth-child(odd) {
+    background: whitesmoke;
+  }
+  li:hover {
+    background: rgb(223, 220, 220);
+    color: firebrick;
+  }
+}
 </style>
